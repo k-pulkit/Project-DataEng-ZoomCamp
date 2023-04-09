7f7106ee46a7d0490d6db624448a1ee19d6cbf5f
k-pulkit <pulkit42041@gmail.com>
2023-04-09 15:27:09 -0400

    Practice and test code for ingestion

---
+++ learn_practice/docker_learning/1. docker_test_pandas/create_tables.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS ny_taxi_data (
+	"VendorID" BIGINT, 
+	tpep_pickup_datetime TIMESTAMP WITHOUT TIME ZONE, 
+	tpep_dropoff_datetime TIMESTAMP WITHOUT TIME ZONE, 
+	passenger_count BIGINT, 
+	trip_distance FLOAT(53), 
+	"RatecodeID" BIGINT, 
+	store_and_fwd_flag TEXT, 
+	"PULocationID" BIGINT, 
+	"DOLocationID" BIGINT, 
+	payment_type BIGINT, 
+	fare_amount FLOAT(53), 
+	extra FLOAT(53), 
+	mta_tax FLOAT(53), 
+	tip_amount FLOAT(53), 
+	tolls_amount FLOAT(53), 
+	improvement_surcharge FLOAT(53), 
+	total_amount FLOAT(53), 
+	congestion_surcharge FLOAT(53)
+)
+
