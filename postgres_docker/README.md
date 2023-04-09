1030cba7449c7ddf242d896839f7ee8ab09ff9fc
k-pulkit <pulkit42041@gmail.com>
2023-04-09 15:28:52 -0400

    Postgres for main application with statup script

---
+++ postgres_docker/README.md
@@ -0,0 +1,5 @@
+## Description
+
+In this, we will be creating a postgres database using docker, the default directory as well as the bash commands to get it up to speed.
+
+We are creating a database for ingesting the records for the NY_TAXI files.
---
+++ postgres_docker/start_postgres.sh
@@ -0,0 +1,17 @@
+
+network_name=my_app_network
+if docker network ls | awk -F ' ' '{print $2}' | grep $network_name; then
+    echo "network exists"
+else 
+    echo "creating required network"
+    docker network create $network_name
+fi
+
+# Run the postgres database, and add it to application network for access 
+
+docker run --rm -itd \
+    -v "$(pwd)/ny_taxi_postgres_data":/var/lib/postgresql/data \
+    -p 5431:5432 \
+    --network my_app_network \
+    --name postgres_db_zoomcamp \
+        test:postgres
